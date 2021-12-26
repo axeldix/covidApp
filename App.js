a4fcc26f71e2e5487059f18acbc4e0ac4797de90
axdiaz <axeldiaz89@gmail.com>
2021-12-26 01:17:51 -0300

    coding list

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
 import 'react-native-gesture-handler';
-import React, {useEffect} from 'react';
+import * as React from 'react';
+import {useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import LogIn from './src/screens/logIn';
 import {createStackNavigator} from '@react-navigation/stack';
-import {View, Text} from 'react-native';
+import {View, Text, ScrollView, FlatList} from 'react-native';
 import axios from 'react-native-axios';
+import {list, shortList} from './list';
 
 const Stack = createStackNavigator();
 
 const ConfirmedCases = () => {
-  return <Text>"ConfirmedCases Screen"</Text>;
+  return (<Text>"ConfirmedCases Screen"</Text>);
 };
 
 const CountryList = () => {
   //  const prettyJson = (value: any) => {
   //    return JSON.stringify(value, null, 2);
   //  };
+  // const [list, setList] = useState(false);
 
   const getList = () => {
     return fetch('https://api.covid19api.com/countries')
@@ -32,64 +35,31 @@ const CountryList = () => {
       });
   };
 
-  // const getMoviesFromApiAsync = async () => {
-  //   try {
-  //     const response = await fetch('https://api.covid19api.com/countries');
-  //     console.log('response', response);
-  //     const json = await response.json();
-  //     return json.movies;
-  //   } catch (error) {
-  //     console.error('getMoviesFromApiAsync', error);
-  //   }
-  // };
-  // const getList = () => {
-  //   const url = 'https://api.covid19api.com/countries';
-  //   axios
-  //     .get(url)
-  //     .then(res => console.log(res.data))
-  //     .catch(err => console.log(err));
-  // };
-  // const getList = () => {
-  //   const authURL = 'https://www.google.com.ar';
-  //   // const authURL = 'http://api.covid19api.com/countries';
-  //   return fetch(authURL, {method: 'GET'})
-  //     .then(
-  //       response => {
-  //         const statusCode = response.status;
-  //         console.warn('status Code', statusCode);
-  //         if (statusCode == 200) {
-  //           //success code
-  //         } else {
-  //           //handle other error code
-  //         }
-  //       },
-  //       err => {
-  //         console.warn('error', err);
-  //       },
-  //     )
-  //     .catch(error => {
-  //       console.error(error);
-  //       return error;
-  //     });
-  // };
   useEffect(() => {
-    const result = getList();
-    console.log('res', result);
+    // const result = getList();
+    // setList(result);
+    // console.log('res', result);
   }, []);
 
-  const list = [
-    {title: 'titulo 1'},
-    {title: 'titulo 2'},
-    {title: 'titulo 3'},
-    {title: 'titulo 1'},
-    {title: 'titulo 2'},
-    {title: 'titulo 3'},
-  ];
   return (
     <View>
-      {list.map(item => (
-        <Text>{item.title}</Text>
-      ))}
+      {/* <FlatList
+        data={shortList}
+        renderItem={({item}) => (
+          <View>
+            <Text>{item.Country}</Text>
+          </View>
+        )}
+      /> */}
+      {/* <ScrollView>
+        {list
+          ? list.map((item, index) => (
+              <View key={index}>
+                <Text>{item.Country}</Text>
+              </View>
+            ))
+          : (<View>asdasdad</View>)}
+      </ScrollView> */}
     </View>
   );
 };
@@ -97,9 +67,9 @@ const CountryList = () => {
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="LogIn" component={LogIn} />
+      <Stack.Navigator initialRouteName="LogIn">
         <Stack.Screen name="CountryList" component={CountryList} />
+        <Stack.Screen name="LogIn" component={LogIn} />
         <Stack.Screen name="ConfirmedCases" component={ConfirmedCases} />
       </Stack.Navigator>
     </NavigationContainer>
